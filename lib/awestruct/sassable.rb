7653765b2fd4eae7ace94516e6bbed6ffa9545ac
Bob McWhirter <bob@fnokd.com>
2010-02-24 17:07:31 -0500

    Marginal integration of compass into Sass rendering.

--- lib/awestruct/sassable.rb
+++ lib/awestruct/sassable.rb
@@ -1,10 +1,19 @@
 require 'sass'
 
+require 'compass'
+
 module Awestruct
   module Sassable
 
     def render(context)
-      sass_engine = Sass::Engine.new( raw_page_content )
+      sass_opts = {
+        :load_paths => [
+          File.dirname( self.source_path ),
+          Compass::Frameworks['compass'].stylesheets_directory,
+          Compass::Frameworks['blueprint'].stylesheets_directory,
+        ],
+      }
+      sass_engine = Sass::Engine.new( raw_page_content, sass_opts )
       sass_engine.render
     end
 
