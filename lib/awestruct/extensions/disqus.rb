4404127e86a728e256bebd478d3bcbe848c58ab9
Torsten Curdt <tcurdt@vafer.org>
2010-10-13 01:15:21 +0200

    seems like disqus_developer is not required

--- lib/awestruct/extensions/disqus.rb
+++ lib/awestruct/extensions/disqus.rb
@@ -8,16 +8,9 @@ module Awestruct
 
       module Disqus
         def disqus_comments()
-          html = %Q{
+          %Q{
             <div id="disqus_thread"></div>
             <script type="text/javascript">
-          }
-          if site.disqus_developer
-            html += %Q{
-              var disqus_developer = 1;
-            }
-          end
-          html += %Q{
             var disqus_url = "#{self.url}";
             (function() {
               var dsq = document.createElement("script"); dsq.type = "text/javascript"; dsq.async = true;
@@ -27,7 +20,6 @@ module Awestruct
             </script>
             <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript=#{site.disqus}">comments powered by Disqus.</a></noscript>
           }
-          html
         end
 
         def disqus_comments_link()
