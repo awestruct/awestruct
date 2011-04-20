688cdb1456f637293c9f7d4c789c0f4566bd9b12
Torsten Curdt <tcurdt@vafer.org>
2011-04-21 01:04:36 +0200

    make url path available to sass, ws

--- lib/awestruct/sassable.rb
+++ lib/awestruct/sassable.rb
@@ -1,6 +1,7 @@
 require 'sass'
 
 require 'compass'
+require 'uri'
 
 module Awestruct
   module Sassable
@@ -11,8 +12,9 @@ module Awestruct
       Compass::Frameworks::ALL.each do |framework|
         sass_opts[:load_paths] << framework.stylesheets_directory
       end
-      sass_opts[:load_paths] << File.dirname( self.source_path ) 
+      sass_opts[:load_paths] << File.dirname( self.source_path )
       sass_opts[:syntax] = syntax()
+      sass_opts[:custom] = URI.parse(site.base_url).path
       sass_engine = Sass::Engine.new( raw_page_content, sass_opts )
       sass_engine.render
     end
