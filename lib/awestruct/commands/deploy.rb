02636949192bfc8570970fd373a89f56c96d7007
Torsten Curdt <tcurdt@vafer.org>
2011-05-27 02:30:00 +0200

    nicer output on deploy

--- lib/awestruct/commands/deploy.rb
+++ lib/awestruct/commands/deploy.rb
@@ -12,19 +12,26 @@ module Awestruct
 
       def run
         cmd = "rsync -r -l -i --no-p --no-g --chmod=Dg+s,ug+w --delete #{@site_path} #{@host}:#{@path}"
-        puts "running #{cmd}"
-        Open3.popen3( cmd ) do |stdin, stdout, stderr| 
+        Open3.popen3( cmd ) do |stdin, stdout, stderr|
           stdin.close
           threads = []
           threads << Thread.new(stdout) do |i|
             while ( ! i.eof? )
-              line = i.readline 
-              puts line
+              line = i.readline
+              case line[0,9]
+              when '<f.sT....'
+                puts " updating #{line[10..-1]}"
+              when '<f..T....'
+                # ignoring unchanged files
+                # puts " no change to #{line[10..-1]}"
+              else
+                puts line
+              end
             end
           end
           threads << Thread.new(stderr) do |i|
             while ( ! i.eof? )
-              line = i.readline 
+              line = i.readline
               puts line
             end
           end
