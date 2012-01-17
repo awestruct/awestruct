35aa07a2784736a5218ab003111c7cd9798fe378
Torsten Curdt <tcurdt@vafer.org>
2012-01-17 18:24:33 +0100

    support for partials parameters

--- lib/awestruct/extensions/partial.rb
+++ lib/awestruct/extensions/partial.rb
@@ -3,12 +3,25 @@ module Awestruct
   module Extensions
     module Partial
 
-      def partial(path)
-        page = site.engine.load_site_page( File.join( '_partials', path ) )
-        page.content if ( page )
+      def partial(path, params = {})
+        filename = File.join( '_partials', path )
+
+        if !File.exists?( filename )
+          puts "no file #{filename} to include"
+          return nil
+        end
+
+        page = site.engine.load_site_page( filename )
+
+        return nil if !page
+
+        params.each do |k,v|
+          page.send( "#{k}=", v )
+        end if params
+
+        page.content
       end
 
     end
   end
-
 end
