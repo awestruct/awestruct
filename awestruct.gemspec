52f9b6a4079c0b74b9a8fb148ef3c3a75acbae3e
Bob McWhirter <bob@mcwhirter.org>
2012-06-06 17:30:16 -0400

    Replace Guard usage with Listen.

--- awestruct.gemspec
+++ awestruct.gemspec
@@ -20,7 +20,6 @@ spec = Gem::Specification.new do |s|
     s.has_rdoc       =   true
 
     s.add_dependency 'hpricot', '~> 0.8.6'
-    s.add_dependency 'versionomy', '~> 0.4.3'
     s.add_dependency 'haml', '~> 3.1.4'
     s.add_dependency 'sass', '~> 3.1.15'
     s.add_dependency 'hashery', '~> 1.5.0'
@@ -36,7 +35,7 @@ spec = Gem::Specification.new do |s|
     s.add_dependency 'notifier', '~> 0.1.4'
     s.add_dependency 'git', '~> 1.2.5'
 
-    s.add_dependency 'guard'
+    s.add_dependency 'listen', '~> 0.4.2'
 
     s.add_development_dependency 'rspec', '~> 2.9'
     s.add_development_dependency 'rake', '~> 0.9.2'
--- lib/awestruct/cli/auto.rb
+++ lib/awestruct/cli/auto.rb
@@ -1,4 +1,6 @@
-require 'guard/awestruct'
+#require 'guard/awestruct'
+
+require 'listen'
 
 module Awestruct
   module CLI
@@ -9,25 +11,26 @@ module Awestruct
       end
 
       def run()
-        Guard.setup
-        Guard.start( :guardfile_contents=>guardfile_contents,
-                     :watchdir=>@config.dir,
-                     :watch_all_modifications=>true )
+        listener = Listen.to( @config.dir, :relative_paths=>true )
+        listener.ignore( %r(\.awestruct) )
+        listener.ignore( %r(^#{File.basename( @config.tmp_dir )}) )
+        listener.ignore( %r(^#{File.basename( @config.output_dir )}) )
+        listener.change do |modified, added, removed|
+          puts "modified #{modified.inspect}"
+          puts "added #{added.inspect}"
+          puts "removed #{removed.inspect}"
+          modified.each do |path|
+            engine = ::Awestruct::Engine.instance
+            puts "path -> #{path}"
+            puts "bn -> #{File.basename( engine.config.output_dir )}"
+            unless ( path =~ %r(#{File.basename( engine.config.output_dir) }) || path =~ /.awestruct/ )
+              engine.generate_page_by_output_path( path )
+            end
+          end
+        end
+        listener.start( false )
       end
 
-      def guardfile_contents
-        ignored = [ 
-          "'.awestruct'",
-          "'#{File.basename( @config.tmp_dir )}'",
-          "'#{File.basename( @config.output_dir )}'",
-        ] 
-        c = ''
-        c += "guard :awestruct do\n"
-        c += "  watch %r(.*)\n"
-        c += "  ignore_paths #{ignored.join(', ')}"
-        c += "end\n"
-        c
-      end
     end
 
   end
