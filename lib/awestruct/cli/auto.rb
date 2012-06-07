a027a1d4d342826bd1ace451c4ac5cab4f3d5643
Bob McWhirter <bob@mcwhirter.org>
2012-06-07 12:27:01 -0400

    Let's remove some debug.

--- lib/awestruct/cli/auto.rb
+++ lib/awestruct/cli/auto.rb
@@ -16,13 +16,8 @@ module Awestruct
         listener.ignore( %r(^#{File.basename( @config.tmp_dir )}) )
         listener.ignore( %r(^#{File.basename( @config.output_dir )}) )
         listener.change do |modified, added, removed|
-          puts "modified #{modified.inspect}"
-          puts "added #{added.inspect}"
-          puts "removed #{removed.inspect}"
           modified.each do |path|
             engine = ::Awestruct::Engine.instance
-            puts "path -> #{path}"
-            puts "bn -> #{File.basename( engine.config.output_dir )}"
             unless ( path =~ %r(#{File.basename( engine.config.output_dir) }) || path =~ /.awestruct/ )
               engine.generate_page_by_output_path( path )
             end
