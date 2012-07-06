6a50fb31c9303ea5cc0484d26b1fdff2ff6da188
Pete Muir <pmuir@bleepbleep.org.uk>
2012-07-06 14:53:25 +0100

    Cache remote partials locally
    * remote fetch every time slows down site 
      generation a lot
    * remote fetch every time breaks aeroplane
      development mode
    * add rest-client dependency to do fetch

--- lib/awestruct/extensions/remotePartial.rb
+++ lib/awestruct/extensions/remotePartial.rb
@@ -1,16 +1,38 @@
 require 'open-uri'
+require 'restclient'
 
 module Awestruct
   module Extensions
     module RemotePartial
 
       def remotePartial(url)
-
-        page = open(url)
-        return nil if !page
-        page.read
-
+        url_tmp = url.sub('http://', '')
+        r = 'remote_partial/' + url_tmp[/(.*)\/[^\/].+$/, 1]
+        tmp = File.join(tmp(site.tmp_dir, r), File.basename(url_tmp))
+        get_or_cache(tmp, url)
       end
+
+      def get_or_cache(tmp_file, url)
+        response_body = ""
+        if !File.exist?tmp_file
+          puts url
+          response_body = RestClient.get(url, :cache => false) { |response, request, result, &block|
+            case response.code
+            when 404
+                response
+            else
+              response.return!(request, result, &block)
+            end
+          }.body;
+          File.open(tmp_file, 'w') do |out|
+            out.write response_body
+          end
+        else
+          response_body = File.read(tmp_file)
+        end
+        return response_body
+      end
+
 
     end
   end
