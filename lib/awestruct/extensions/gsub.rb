6d627a93356ed7f4831cd44c2b190c69ae326a78
Torsten Curdt <tcurdt@vafer.org>
2012-08-11 20:48:09 +0200

    lambda for replacement, array for the common extension matchin

--- lib/awestruct/extensions/gsub.rb
+++ lib/awestruct/extensions/gsub.rb
@@ -4,17 +4,31 @@ module Awestruct
 
       def initialize(pattern, replacement, options = {})
         @pattern = pattern
-        @replacement = replacement
-        @gsub_required = options[:gsub_required] || lambda { |site, page| page.output_path.end_with?(".html") }
+	@replacement = replacement.is_a?(Proc) ? replacement : lambda { |site, page| replacement }
+	@gsub_required = lambdaize(options[:gsub_required])
       end
 
       def transform(site, page, rendered)
         if (@gsub_required.call(site, page))
-          rendered = rendered.gsub(@pattern, @replacement)
+	  replacement = @replacement.call(site, page).to_s
+	  rendered = rendered.gsub(@pattern, replacement)
         end
         rendered
       end
 
+      private
+
+      def lambdaize(param)
+	if param.nil?
+	  lambdaize([".html"])
+	else
+	  if param.is_a?(Array)
+	    lambda { |site, page| param.any?{ |ext| page.output_path.end_with?(ext) } }
+	  else
+	    param
+	  end
+	end
+      end
     end
   end
 end
