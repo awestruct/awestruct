2896d25dd174c389b96652592cf6568359ad74dd
Torsten Curdt <tcurdt@vafer.org>
2012-08-11 20:49:44 +0200

    page and site based cachebuster on page object

--- lib/awestruct/extensions/cachebuster.rb
+++ lib/awestruct/extensions/cachebuster.rb
@@ -1,11 +1,22 @@
 module Awestruct
   module Extensions
     module Cachebuster
-      def timestamp
-        (Time.now.to_i / 1000).to_i
-      end
+
       def cache(href)
-        "#{href}?#{site.cachebuster||timestamp}"
+	"#{href}?#{cachebuster}"
+      end
+
+      class Extension
+
+	def execute(site)
+	  site.pages.each{ |p| p.extend Extension }
+	end
+
+	module Extension
+	  def cachebuster
+	    ((site.timestamp || page.input_mtime || Time.now.to_i) / 1000).to_i.to_s
+	  end
+	end
       end
     end
   end
