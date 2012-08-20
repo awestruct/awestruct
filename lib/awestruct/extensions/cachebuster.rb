8789c9d0772ad79b737658683597d28f11916b18
Torsten Curdt <tcurdt@vafer.org>
2012-08-21 01:02:31 +0200

    helper no extension

--- lib/awestruct/extensions/cachebuster.rb
+++ lib/awestruct/extensions/cachebuster.rb
@@ -3,21 +3,13 @@ module Awestruct
     module Cachebuster
 
       def cache(href)
-        "#{href}?#{page.cachebuster}"
+	"#{href}?#{cachebuster}"
       end
 
-      class Extension
-
-        def execute(site)
-          site.pages.each{ |p| p.extend Extension }
-        end
-
-        module Extension
-          def cachebuster
-            ((site.timestamp || page.input_mtime || Time.now.to_i) / 1000).to_i.to_s
-          end
-        end
+      def cachebuster(p=page)
+	((site.timestamp || p.input_mtime || Time.now.to_i) / 1000).to_i.to_s
       end
+
     end
   end
 end
