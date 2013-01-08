796da0b329a3233f5deb15643b96228fcf64cd53
Xavier Coulon <xcoulon@redhat.com>
2013-01-08 20:36:32 +0100

    Adding 'selected_tag' as an argument to the paginator
    This 'selected_tag' variable becomes available in the page context if
    the page is generated by the tagger. This enables the generated page
    to display the current tag in the page body.
    
    For example, if /news/tags/awestruct/index.html is generated, the page
    can display somewhere in its body that theselected tag is: "awestruct"

--- lib/awestruct/extensions/paginator.rb
+++ lib/awestruct/extensions/paginator.rb
@@ -13,7 +13,7 @@ module Awestruct
         def links
           html = %Q(<div class="pagination-links">)
           unless ( previous_page.nil? )
-            html += %Q(<a href="#{previous_page.url}" class="previous-link">Previous</a> )
+            html += %Q(<a href="#{site.base_url}#{previous_page.url}" class="previous-link">Previous</a> )
           end
           first_skip = false
           second_skip = false
@@ -21,7 +21,7 @@ module Awestruct
             if ( i == current_page_index )
               html += %Q(<span class="current-page">#{i+1}</span> )
             elsif ( i <= window )
-              html += %Q(<a href="#{page.url}" class="page-link">#{i+1}</a> )
+              html += %Q(<a href="#{site.base_url}#{page.url}" class="page-link">#{i+1}</a> )
             elsif ( ( i > window ) && ( i < ( current_page_index - window ) ) && ! first_skip  )
               html += %Q(<span class="skip">...</span>)
               first_skip = true
@@ -29,13 +29,13 @@ module Awestruct
               html += %Q(<span class="skip">...</span>)
               second_skip = true
             elsif ( ( i >= ( current_page_index - window ) ) && ( i <= ( current_page_index + window ) ) )
-              html += %Q(<a href="#{page.url}" class="page-link">#{i+1}</a> )
+              html += %Q(<a href="#{site.base_url}#{page.url}" class="page-link">#{i+1}</a> )
             elsif ( i >= ( ( pages.size - window ) - 1 ) )
-              html += %Q(<a href="#{page.url}" class="page-link">#{i+1}</a> )
+              html += %Q(<a href="#{site.base_url}#{page.url}" class="page-link">#{i+1}</a> )
             end
           end
           unless ( next_page.nil? )
-            html += %Q(<a href="#{next_page.url}" class="next-link">Next</a> )
+            html += %Q(<a href="#{site.base_url}#{next_page.url}" class="next-link">Next</a> )
           end
           html += %Q(</div>)
           html
@@ -52,6 +52,9 @@ module Awestruct
         @output_prefix = opts[:output_prefix] || File.dirname( @input_path )
         @page_name     = opts[:page_name] || 'page/'
         @collection    = opts[:collection]
+        if opts[:selected_tag]
+          @selected_tag    = opts[:selected_tag]
+        end
       end
 
       def execute(site)
@@ -70,6 +73,7 @@ module Awestruct
             page.output_path = File.join( @output_prefix, "#{@page_name}#{i}.html" )
           end
           page.paginate_generated = true
+          page.selected_tag = @selected_tag
           site.pages << page
           paginated_pages << page
           i = i + 1
