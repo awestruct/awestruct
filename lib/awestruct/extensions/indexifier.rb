2ab3f9370f8a5babe518eb1570219a405ec9f85d
LightGuard <lightguard.jp@gmail.com>
2013-06-03 14:15:42 -0600

    Turn the method into a one liner

--- lib/awestruct/extensions/indexifier.rb
+++ lib/awestruct/extensions/indexifier.rb
@@ -4,31 +4,18 @@ module Awestruct
 
       def initialize(exclude_regex_paths = [])
         @exclude_regex_paths = exclude_regex_paths
+        @exclude_regex_paths << /^(.*\/)?index.html$/
       end
 
       def execute(site)
         site.pages.each do |page|
-          if ( page.inhibit_indexifier ||  excluded_path(page) || ( page.output_path =~ /^(.*\/)?index.html$/ ) )
+          if ( page.inhibit_indexifier ||  Regexp.union(@exclude_regex_paths).match(page.output_path) )
             # skip it!
           else
             page.output_path = page.output_path.gsub( /.html$/, '/index.html' )
           end
         end
-      end
-
-      def excluded_path(page)
-        if (@exclude_regex_paths == nil)
-          return false
-        else
-          @exclude_regex_paths.each do |regex_path|
-             if (page.output_path.match(regex_path) != nil)
-               return true
-             end
-          end
-          return false
-       end
-      end
-
+      end 
     end
   end
 end
