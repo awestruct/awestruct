0ae70cef678f30e12eb3e24aa69849da0fc7aca7
LightGuard <lightguard.jp@gmail.com>
2013-07-31 11:49:21 -0600

    Removes testing deprecations
    Also some reformating

--- spec/mustache_handler_spec.rb
+++ spec/mustache_handler_spec.rb
@@ -7,23 +7,27 @@ verify = lambda { |output|
 theories =
   [
     {
-      :page => "mustache-page.html.mustache",
-      :simple_name => "mustache-page",
+      :page => 'mustache-page.html.mustache',
+      :simple_name => 'mustache-page',
       :syntax => :mustache,
       :extension => '.html',
       :matcher => verify
     },
     {
-      :page => "mustache-page.xml.mustache",
-      :simple_name => "mustache-page",
+      :page => 'mustache-page.xml.mustache',
+      :simple_name => 'mustache-page',
       :syntax => :mustache,
       :extension => '.xml',
       :matcher => verify
     }
   ]
 
-describe Awestruct::Handlers::TiltHandler.to_s + "-Mustache" do
-  let(:additional_config) { {:markup_type => 'Mustache'} }
-  it_should_behave_like "a handler", theories
+
+describe Awestruct::Handlers::TiltHandler.to_s + '-Mustache' do
+  def additional_config
+    {:markup_type => 'Mustache'}
+  end
+
+  it_should_behave_like 'a handler', theories
 
 end
