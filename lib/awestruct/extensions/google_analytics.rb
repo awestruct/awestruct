63299491bddc234c712b5ffb747c81405686c058
Torsten Curdt <tcurdt@vafer.org>
2014-03-13 16:49:26 +0100

    clean up GA support, add new universal tracker https://github.com/awestruct/awestruct/pull/400 and added bounceTime option

--- lib/awestruct/extensions/google_analytics.rb
+++ lib/awestruct/extensions/google_analytics.rb
@@ -1,30 +1,32 @@
+# in your site.yml specify
+#
+# google_analytics:
+#  account: UA-something
+#  anonymizeIp: true
+#  bounceTime: 15
+#
+# of course only "account" is required
+
 module Awestruct
   module Extensions
     module GoogleAnalytics
 
-      def google_analytics()
-        html = ''
-        html += %Q(<script type="text/javascript">\n)
-        html += %Q(var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");\n)
-        html += %Q(document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));\n)
-        html += %Q(</script>\n)
-        html += %Q(<script type="text/javascript">\n)
-        html += %Q(try {\n)
-        html += %Q(var pageTracker = _gat._getTracker("#{site.google_analytics}");\n)
-        html += %Q(pageTracker._trackPageview();\n)
-        html += %Q(} catch(err) {}</script>\n)
-        html
+      def google_analytics_async()
+        # deprecated
+        puts "WARNING: google_analytics_async is deprecated. Please use google_analytics or google_analytics_universal."
+        google_analytics()
       end
 
-      def google_analytics_async()
+      def google_analytics(options={})
+        options = defaults(options)
+
         html = ''
         html += %Q(<script type="text/javascript">\n)
         html += %Q(var _gaq = _gaq || [];\n)
-        html += %Q(_gaq.push(['_setAccount','#{site.google_analytics}']);\n)
-        if site.google_analytics_anonymize
-          html += %Q(_gaq.push(['_gat._anonymizeIp']);\n)
-        end
+        html += %Q(_gaq.push(['_setAccount','#{options[:account]}']);\n)
+        html += %Q(_gaq.push(['_gat._anonymizeIp']);\n) if options[:anonymizeIp]
         html += %Q(_gaq.push(['_trackPageview']);\n)
+        html += %Q(setTimeout("_gaq.push(['_trackEvent','#{options[:bounceTime]}_seconds','read'])", #{options[:bounceTime]}000);\n) if options[:bounceTime]
         html += %Q[(function() {\n]
         html += %Q( var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;\n)
         html += %Q( ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';\n)
@@ -33,6 +35,44 @@ module Awestruct
         html
       end
 
+      def google_analytics_universal(options={})
+        options = defaults(options)
+
+        html = ''
+        html += %Q(<script>\n)
+        html += %Q( (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){\n
+                    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),\n
+                    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)\n
+                    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');\n )
+        html += %Q(\n)
+        html += %Q(ga('create', '#{options[:account]}', 'auto');\n)
+        html += %Q(ga('send', 'pageview');\n)
+        html += %Q(ga('set', 'anonymizeIp', true);\n) if options[:anonymizeIp]
+        html += %Q(setTimeout("ga('send', 'event', 'read', '#{options[:bounceTime]} seconds')", #{options[:bounceTime]}000);\n) if options[:bounceTime]
+        html += %Q(\n)
+        html += %Q(</script>\n)
+        html
+      end
+
+      private
+
+      def defaults(options)
+        options = site.google_analytics.merge(options) if site.google_analytics.is_a?(Hash)
+        options = Hash[options.map{ |k, v| [k.to_sym, v] }]
+
+        if site.google_analytics_anonymize
+          # deprecated
+          puts "WARNING: Syntax has changed to site.google_analytics = { :anonymizeIp => true }"
+          options[:anonymizeIp] = true
+        end
+        if site.google_analytics.is_a?(String)
+          # deprecated
+          puts "WARNING: Syntax has changed to site.google_analytics = { :account => '#{site.google_analytics}' }"
+          options[:account] = site.google_analytics
+        end
+
+        options
+      end
     end
   end
 end
