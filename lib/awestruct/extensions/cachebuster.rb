cc7fbaf63a2b11c1fb4208b7cfca855fa693beaa
Torsten Curdt <tcurdt@vafer.org>
2014-03-13 16:49:43 +0100

    formatting

--- lib/awestruct/extensions/cachebuster.rb
+++ lib/awestruct/extensions/cachebuster.rb
@@ -3,11 +3,11 @@ module Awestruct
     module Cachebuster
 
       def cache(href)
-	"#{href}?#{cachebuster}"
+	      "#{href}?#{cachebuster}"
       end
 
       def cachebuster(p=page)
-	((site.timestamp || p.input_mtime || Time.now.to_i) / 1000).to_i.to_s
+	      ((site.timestamp || p.input_mtime || Time.now.to_i) / 1000).to_i.to_s
       end
 
     end
