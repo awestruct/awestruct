0e5a4fbdc9d0d94a658af30837d1ca410004081a
LightGuard <lightguard.jp@gmail.com>
2014-04-16 12:26:30 -0600

    Last bit of test moving
    We should now be in a better, more typical location for our tests.

--- spec/awestruct/handlers/file_handler_spec.rb
+++ spec/awestruct/handlers/file_handler_spec.rb
@@ -3,15 +3,26 @@ require 'hashery'
 require 'awestruct/handlers/file_handler'
 
 describe Awestruct::Handlers::FileHandler do
+  class TestTransformer
+    def transform(site, page, rendered)
+      rendered.gsub( /howdy/, 'adios' )
+    end
+  end 
 
   before :all do
     @site = Hashery::OpenCascade[ { :encoding=>false, :dir=>Pathname.new( test_data_dir '' )}]
+    @site.engine = Hashery::OpenCascade[]
+  end
+
+  before :each do
+    @filename = Pathname.new( test_data_dir 'simple-file.txt' )
+    @handler = Awestruct::Handlers::FileHandler.new( @site, @filename )
+    @page = Awestruct::Page.new( @site, @handler )
+    @site.engine.pipeline = Awestruct::Pipeline.new
   end
 
   it "should be able to read a valid absolute file handler" do
-    filename = Pathname.new( test_data_dir 'simple-file.txt' )
-    handler = Awestruct::Handlers::FileHandler.new( @site, filename )
-    handler.raw_content.strip.should == 'howdy'
+    @handler.raw_content.strip.should == 'howdy'
   end
 
   it "should be able to read a valid relative file handler" do
@@ -22,35 +33,44 @@ describe Awestruct::Handlers::FileHandler do
   end
 
   it "should be stale before being read" do
-    filename = Pathname.new( test_data_dir 'simple-file.txt' )
-    handler = Awestruct::Handlers::FileHandler.new( @site, filename )
-    handler.should be_stale
+    @handler.should be_stale
   end
 
   it "should not be stale after being read" do
-    filename = Pathname.new( test_data_dir 'simple-file.txt' )
-    handler = Awestruct::Handlers::FileHandler.new( @site, filename )
-    handler.raw_content.strip.should == 'howdy'
-    handler.should_not be_stale
+    @handler.raw_content.strip.should == 'howdy'
+    @handler.should_not be_stale
   end
 
   it "should be stale if touched after being read" do
-    filename = Pathname.new( test_data_dir 'simple-file.txt' )
-    handler = Awestruct::Handlers::FileHandler.new( @site, filename )
-    handler.raw_content.strip.should == 'howdy'
-    handler.should_not be_stale
+    @handler.raw_content.strip.should == 'howdy'
+    @handler.should_not be_stale
     sleep(1)
-    FileUtils.touch( filename )
-    handler.should be_stale
+    FileUtils.touch( @filename )
+    @handler.should be_stale
   end
 
   it "should provide reasonable paths" do
-    filename = Pathname.new( test_data_dir 'simple-file.txt' )
-    handler = Awestruct::Handlers::FileHandler.new( @site, filename )
-    handler.relative_source_path.should == '/simple-file.txt'
-    handler.output_filename.should == 'simple-file.txt'
-    handler.output_extension.should == '.txt'
-    handler.output_path.should == '/simple-file.txt'
+    @handler.relative_source_path.should == '/simple-file.txt'
+    @handler.output_filename.should == 'simple-file.txt'
+    @handler.output_extension.should == '.txt'
+    @handler.output_path.should == '/simple-file.txt'
+  end
+
+  it "should be able to create an appropriate context" do
+    context = @page.create_context
+    context.site.should == @site
+    context.page.should == @page
+    context.content.should == ''
+  end
+
+  it "should allow relative_source_path to be assignable" do
+    @page.relative_source_path = '/taco'
+    @page.relative_source_path.should == '/taco'
+  end
+
+  it "should not apply transformers, even if present" do
+     @site.engine.pipeline.transformer TestTransformer.new
+     @page.rendered_content.strip.should == 'howdy'
   end
 
 end
--- spec/awestruct/page_loader_spec.rb
+++ spec/awestruct/page_loader_spec.rb
@@ -60,4 +60,37 @@ describe Awestruct::PageLoader do
 
   end
 
+  context 'with layouts' do
+    before :each do
+      @opts = Awestruct::CLI::Options.new
+      @opts.source_dir = test_data_dir 'page-loader' 
+      @config = Awestruct::Config.new( @opts )
+      @engine = Awestruct::Engine.new
+      @engine.pipeline.handler_chains << :defaults
+      @site   = Awestruct::Site.new( @engine, @config )
+      @loader = Awestruct::PageLoader.new( @site, :layouts )
+    end
+
+    it "should be able to load a site layout" do
+      page = @loader.load_page( File.join( @config.dir, "_layouts", "layout-one.md" ) )
+      page.should_not be_nil
+      page.handler.to_chain.collect{|e| e.class}.should be_include Awestruct::Handlers::TiltHandler
+      page.relative_source_path.to_s.should == "/_layouts/layout-one.md" 
+    end
+
+    it "should be able to load all site layouts" do
+      @loader.load_all
+      @site.layouts.size.should == 2
+
+      @site.layouts.sort!{|l,r| l.relative_source_path <=> r.relative_source_path }
+
+      @site.layouts[0].relative_source_path.should == '/_layouts/layout-one.md'
+      @site.layouts[0].output_path.should          == '/_layouts/layout-one.html'
+
+      @site.layouts[1].relative_source_path.should == '/_layouts/layout-two.html.haml'
+      @site.layouts[1].output_path.should          == '/_layouts/layout-two.html'
+    end
+
+  end
+
 end
