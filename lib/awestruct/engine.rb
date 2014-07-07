6577179731675dc5a612c7ee8017507c42cd9a53
LightGuard <lightguard.jp@gmail.com>
2014-07-07 17:27:00 -0600

    Fine grained page timings

--- lib/awestruct/engine.rb
+++ lib/awestruct/engine.rb
@@ -296,12 +296,14 @@ module Awestruct
     def generate_output
       FileUtils.mkdir_p( site.config.output_dir )
       @site.pages.each do |page|
+        start_time = DateTime.now
         generated_path = File.join( site.config.output_dir, page.output_path )
         if ( page.stale_output?( generated_path ) )
           generate_page( page, generated_path )
         else
           generate_page( page, generated_path, false )
         end
+        puts "Total time generating page #{generated_path}: #{DateTime.now.to_time - start_time.to_time} seconds"
       end
     end
 
