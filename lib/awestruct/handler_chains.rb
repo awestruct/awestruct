0d24bc1acfd41b89fa76dd20d5443b8f5506b523
LightGuard <lightguard.jp@gmail.com>
2014-10-24 16:25:18 -0600

    Further removing rst handler

--- lib/awestruct/handler_chains.rb
+++ lib/awestruct/handler_chains.rb
@@ -1,12 +1,9 @@
 require 'awestruct/handler_chain'
 require 'awestruct/handlers/verbatim_file_handler'
 require 'awestruct/handlers/css_tilt_handler'
-require 'awestruct/handlers/restructuredtext_handler'
 require 'awestruct/handlers/javascript_handler'
 require 'awestruct/handlers/redirect_handler'
 require 'awestruct/handlers/tilt_handler'
-# TC: shouldn't the asciidoctor be covered by the tilt handler?
-# JP: We have some additional asciidoctor integration that the tilt handler doesn't handle (yet, working on it)
 require 'awestruct/handlers/asciidoctor_handler'
 
 module Awestruct
@@ -16,7 +13,6 @@ module Awestruct
     DEFAULTS = [
       Awestruct::Handlers::CssTiltHandler::CHAIN,
       Awestruct::Handlers::RedirectHandler::CHAIN,
-      Awestruct::Handlers::RestructuredtextHandler::CHAIN,
       Awestruct::Handlers::JavascriptHandler::CHAIN,
       Awestruct::Handlers::AsciidoctorHandler::CHAIN,
       Awestruct::Handlers::TiltHandler::NON_INTERPOLATION_CHAIN,
