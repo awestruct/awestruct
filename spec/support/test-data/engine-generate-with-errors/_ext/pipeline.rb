5dbe512680b7aa094598ecdcbb6e52d3e75aa32f
LightGuard <lightguard.jp@gmail.com>
2015-04-13 17:32:02 -0600

    Fixing tests

--- spec/support/test-data/engine-generate-with-errors/_ext/pipeline.rb
+++ spec/support/test-data/engine-generate-with-errors/_ext/pipeline.rb
@@ -1,3 +1,5 @@
+require 'awestruct/extensions/pipeline'
+
 Awestruct::Extensions::Pipeline.new do
   # extension Awestruct::Extensions::Posts.new '/news'
   # extension Awestruct::Extensions::Indexifier.new
