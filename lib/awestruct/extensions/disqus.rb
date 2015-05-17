794a8db842573b7c9847f0ea38069cfae634a285
Emmanuel Bernard <emmanuel@hibernate.org>
2015-05-17 17:59:42 +0200

    Make Disqus and Flattr HTTPS friendly

--- lib/awestruct/extensions/disqus.rb
+++ lib/awestruct/extensions/disqus.rb
@@ -24,7 +24,7 @@ module Awestruct
             var disqus_identifier = #{identifier};
             (function() {
               var dsq = document.createElement("script"); dsq.type = "text/javascript"; dsq.async = true;
-              dsq.src = "http://#{site.disqus}.disqus.com/embed.js";
+              dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
               (document.getElementsByTagName("head")[0] || document.getElementsByTagName("body")[0]).appendChild(dsq);
             })();
             </script>
@@ -46,7 +46,7 @@ module Awestruct
             var disqus_shortname = '#{site.disqus}';
             (function () {
               var s = document.createElement('script'); s.async = true;
-              s.src = "http://disqus.com/forums/#{site.disqus}/count.js";
+              s.src = '//' + disqus_shortname + '.disqus.com/count.js';
               (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
             }());
             </script>
