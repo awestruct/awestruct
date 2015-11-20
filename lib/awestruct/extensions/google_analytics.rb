be2a45ca0cf1a5bd7bdf64d79923a6b6034d84bb
a4z <harald.achitz@gmail.com>
2015-11-20 11:35:40 +0100

    remove extra newline in %Q{} which has already new lines

--- lib/awestruct/extensions/google_analytics.rb
+++ lib/awestruct/extensions/google_analytics.rb
@@ -51,9 +51,9 @@ module Awestruct
 
         html = ''
         html += %Q(<script>\n)
-        html += %Q( (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){\n
-                    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),\n
-                    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)\n
+        html += %Q( (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
                     })(window,document,'script','//www.google-analytics.com/analytics.js','ga');\n )
         html += %Q(\n)
         html += %Q(ga('create', '#{options[:account]}', 'auto');\n)
