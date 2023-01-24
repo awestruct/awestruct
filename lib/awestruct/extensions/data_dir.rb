e1e9ba572bee2e7950fc80121fa90fe30c83ac44
Zbynek Konecny <zbynek@geogebra.org>
2023-01-24 01:17:01 +0100

    Use datadir extension from ibeams

--- lib/awestruct/extensions/data_dir.rb
+++ lib/awestruct/extensions/data_dir.rb
@@ -1,3 +1,5 @@
+require 'awestruct/util/yaml_load'
+
 module Awestruct
   module Extensions
     class DataDir
@@ -11,18 +13,22 @@ module Awestruct
       end
 
       def execute(site)
-        Dir[ "#{site.dir}/#{@data_dir}/*" ].each do |entry|
-          if ( File.directory?( entry ) )
-            data_key = File.basename( entry )
-            data_map = {}
-            Dir[ "#{entry}/*" ].each do |chunk|
-              File.basename( chunk ) =~ /^([^\.]+)/
-              key = $1.to_sym
-              chunk_page = site.engine.load_page( chunk )
-              data_map[ key ] = chunk_page
+        Dir.glob(File.join(site.dir, @data_dir, '*')).each do |entry|
+          next unless File.directory? entry
+          data_key = File.basename(entry)
+          data_map = {}
+          Dir.glob(File.join(entry, '*')).each do |chunk|
+            File.basename(chunk) =~ /^([^\.]+)/
+            key = $1.to_sym
+            chunk_page = nil
+            if chunk.end_with?('.yml')
+              chunk_page = Awestruct.yaml_load_file(chunk)
+            else
+              chunk_page = site.engine.load_page(chunk)
             end
-            site.send( "#{data_key}=", data_map )
+            data_map[key] = chunk_page
           end
+          site.send("#{data_key}=", data_map)
         end
       end
 
--- lib/awestruct/util/yaml_load.rb
+++ lib/awestruct/util/yaml_load.rb
@@ -5,4 +5,10 @@ module Awestruct
     return YAML.load(str) unless YAML.method('load').parameters.any? {|k,v| v == :permitted_classes}
     YAML.load(str, permitted_classes: [Date, Symbol])  
   end
+
+  def self.yaml_load_file(str)
+    return YAML.load_file(str) unless YAML.method('load_file').parameters.any? {|k,v| v == :permitted_classes}
+    YAML.load_file(str, permitted_classes: [Date, Symbol])
+  end
+
 end
