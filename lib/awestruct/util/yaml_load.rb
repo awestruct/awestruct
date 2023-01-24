4d1ac55b0fec1a7d46abd381d8e675bba4cdadda
Zbynek Konecny <zbynek@geogebra.org>
2023-01-25 00:26:14 +0100

    Use datadir extension from ibeams - fix yaml load

--- lib/awestruct/util/yaml_load.rb
+++ lib/awestruct/util/yaml_load.rb
@@ -7,7 +7,7 @@ module Awestruct
   end
 
   def self.yaml_load_file(str)
-    return YAML.load_file(str) unless YAML.method('load_file').parameters.any? {|k,v| v == :permitted_classes}
+    return YAML.load_file(str) unless YAML.method('load').parameters.any? {|k,v| v == :permitted_classes}
     YAML.load_file(str, permitted_classes: [Date, Symbol])
   end
 
